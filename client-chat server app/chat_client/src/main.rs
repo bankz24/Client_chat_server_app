8a66855c3f1597867272f00243148bb3ec49abc3
Idoko Edward <idokoedward24@gmail.com>
2022-07-11 02:15:31 +0100

    Add files via upload

---
+++ client-chat server app/chat_client/src/main.rs
@@ -0,0 +1,66 @@
+use std::io::{self, ErrorKind, Read,Write};
+use std::net::TcpStream;
+use std::sync::mpsc::{self,TryRecvError};
+use std::thread;
+use std::time::Duration;
+
+const LOCAL: &str = "127.0.0.1:6000";
+const MSG_SIZE: usize = 32;
+
+
+
+
+fn main() {
+    let mut client  = TcpStream::connect(LOCAL).expect("Stream failed to connect");
+    client.set_nonblocking(true).expect("Failed to initiate non-blocking");
+
+    let(tx,rx) =mpsc::channel::<String>();
+
+    thread::spawn(move || loop {
+        let mut buff = vec![0;MSG_SIZE];
+        match client.read_exact(&mut buff){
+            Ok(_) =>{
+                let msg = buff.into_iter().take_while(|&x | x !=0).collect::<Vec<_>>();
+                println!("Message received {:?}",msg);
+
+            }
+            Err(ref err) if err.kind() == ErrorKind::WouldBlock =>(),
+            Err(_) => {
+                println!("Connection with server was severed");
+                break;
+            }
+
+        }
+        match rx.try_recv(){
+            Ok(msg) => {
+                let mut buff =msg.clone().into_bytes();
+                buff.resize(MSG_SIZE,0);
+                client.write_all(&buff).expect("Writing to socket failed");
+                println!("Message sent {:?}",msg);
+            },
+            Err(TryRecvError::Empty) =>(),
+            Err(TryRecvError::Disconnected) => break
+        }
+        thread::sleep(Duration::from_millis(150));
+    });
+
+    println!("Write a message:");
+    loop {
+        let mut buff = String::new();
+        io::stdin().read_line(&mut buff).expect("Reading from stdin failed");
+
+        let msg = buff.trim().to_string();
+        if msg ==":quit" || tx.send(msg).is_err() {break}
+    }
+
+    println!("Goodbye");
+
+
+
+}
+
+
+
+
+
+
